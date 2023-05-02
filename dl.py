f0c8bdff82fbf8fd63132943ffb9710ea3ae6958
Aarav Singh <aaravzen@gmail.com>
2023-05-02 16:34:26 -0400

    Update dl.py

--- dl.py
+++ dl.py
@@ -4,20 +4,20 @@ def download_file(file_url):
     r = requests.get(file_url, stream = True)
 
     fname = file_url.split("/")[-1]
-    print(fname)
     with open(fname,"wb") as dl:
         for chunk in r.iter_content(chunk_size=1024):
             # writing one chunk at a time to dl
             if chunk:
                 dl.write(chunk)
 
-def get_urls():
-    with open("soup.links.out", "r") as f:
+def get_urls(address="download_these.in"):
+    with open(address, "r") as f:
         lines = f.readlines()
 
     return [x.strip() for x in lines]
 
 if __name__ == '__main__':
     urls = get_urls()
+    print(urls)
     for url in urls:
         download_file(url)
